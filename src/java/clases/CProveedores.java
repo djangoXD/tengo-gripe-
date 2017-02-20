be8be4c2cb7673cbecf1c443696f95181aa0a73c
djangoXD <alexia.12.no@outlook.es>
2017-02-20 17:58:06 -0500

    categoria_prov

--- src/java/clases/CProveedores.java
+++ src/java/clases/CProveedores.java
@@ -19,9 +19,7 @@ public class CProveedores {
     "razon_social",
     "descripcion"
             };
-/*
-
-*/
+    
     public int n=4;
     public String valor[];
 
@@ -34,7 +32,7 @@ public class CProveedores {
 
     @Override
     public String toString() {
-        return "PROVEEDOR "+valor[2];
+        return ("PROVEED"+valor[2]+valor[3]);
     }
 
 }
